abe6b37514a5d113c4f8fe402f2f0da8563adc61
Yevhen Horbachov <killabee1985@gmail.com>
2017-09-21 12:51:10 +0200

    add toolbar to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdToolbarModule, MdCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
+    MdToolbarModule, MdCardModule,
     AppRoutingModule
   ],
   providers: [],
